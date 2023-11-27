4a935e9fa15906088eaa20c9be4b01e8d9a08f76
JustYe <341153946@gapps.yrdsb.ca>
2023-11-27 13:43:44 -0500

    Added animation timer

--- Elephant.java
+++ Elephant.java
@@ -13,7 +13,7 @@ public class Elephant extends Actor
     GreenfootImage[] idleLeft = new GreenfootImage[8];
     
     String facing = "right";
-    
+    SimpleTimer animationTimer = new SimpleTimer();
     /**
      * Constructor - the code that gets run one time when an object is created.
      */
@@ -31,6 +31,8 @@ public class Elephant extends Actor
            idleLeft[i].mirrorHorizontally();
            idleLeft[i].scale(50,50); 
         }
+        
+        animationTimer.mark();
         setImage(idleRight[0]);
     }
     
@@ -40,6 +42,12 @@ public class Elephant extends Actor
     int imageIndex = 0;
     public void animateElephant()
     {
+        if(animationTimer.millisElapsed() < 200)
+        {
+            return;
+        }
+        animationTimer.mark();
+        
         if(facing.equals("right"))
         {
             setImage(idleRight[imageIndex]);
